0686fe42506fc1d09c594ad3eb3c8949a3e41b58
fourstix <>
2021-06-25 09:32:21 -0400

    Updated Make_STG batch file

--- utils/stg_rom/Make_Stg.bat
+++ utils/stg_rom/Make_Stg.bat
@@ -1,3 +1,3 @@
-romtext -a38912 help.new help.hex
-rommerge -s32k -o32768 merged.hex boots.hex video.hex bios.hex sedit.hex edtasm.hex rcbasic.hex visual02.hex xmodem.hex help.hex 
+romtext -a38144 help.new help.hex
+rommerge -s32k -o32768 merged.hex boots.hex video.hex bios.hex sedit.hex forth.hex edtasm.hex rcbasic.hex visual02.hex xmodem.hex help.hex 
 romcksum merged.hex -s32k -o32768 -c32764 StgVideo.hex
